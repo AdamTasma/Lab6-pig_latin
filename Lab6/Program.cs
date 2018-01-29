a452ea8a4fd72fd5e8744efb4ed9553869f2bcb8
Adament13 <adamtasma@gmail.com>
2018-01-29 00:48:45 -0500

    does all extended exercises!

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Globalization;
+//there can be 14 different types of inputs.
+//word ends up having one of eleven different outputs.
 
 namespace Lab6
 {
@@ -27,7 +30,7 @@ namespace Lab6
             for (int i = 0; i < sentence.Length; i++)
             {
                 word = sentence[i];
-                word = ExceptionsPunctuation(word);
+                word = ExceptionsCase(word);
                 sentence[i] = word;
             }
             Console.WriteLine(string.Concat(sentence));
@@ -35,24 +38,54 @@ namespace Lab6
             Repeat();
         }
 
-        //static string ExceptionsCase(string word)
-        //{
-        //    bool isUpperCase;
-        //    char[] letters = word.ToCharArray();
-        //    if (char.IsUpper(letters[0]))
-        //    {
-        //        isUpperCase = true;
-        //    }
-        //    else
-        //    {
-        //        isUpperCase = false;
-        //    }
-        //}
-
-        static string ExceptionsPunctuation(string word)
+        static string ExceptionsCase(string word)
         {
-            word = word.ToLower();
-            bool hasPunctuation;
+            bool isAllCaps = false;
+            bool isTitleCase = false;
+            char[] letters = word.ToCharArray();
+
+            isAllCaps = IsAllCaps(letters);
+            if (isAllCaps)
+            {
+                word = ExceptionsPunctuation(word, isAllCaps, isTitleCase);
+            }
+            else
+            {
+                if ((char.IsUpper(letters[0])) && (isAllCaps == false))
+                {
+                    isTitleCase = true;
+                    //Console.WriteLine("input is title case");
+                    //Console.WriteLine("input is not all lowercase");
+                    word = ExceptionsPunctuation(word, isAllCaps, isTitleCase);
+                }
+                else
+                {
+                    word = ExceptionsPunctuation(word, isAllCaps, isTitleCase);
+                }
+            }
+            return word;
+        }
+
+        static bool IsAllCaps(char[] letters)
+        {
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (char.IsLower(letters[i]))
+                {
+                    //Console.WriteLine("input is not all caps");
+                    return false;
+                }
+            }
+            //Console.WriteLine("input is all caps");
+            return true;
+        }
+
+
+        static string ExceptionsPunctuation(string word, bool isAllCaps, bool isTitleCase)
+        {
+            //in case i can't figure out the casechecking, uncomment the below line.
+            //word = word.ToLower();
+            bool hasPunctuation = false;
             string punctuation;
 
             if ((word.EndsWith(".") || word.EndsWith(",")) || (word.EndsWith("!") || word.EndsWith("?")))
@@ -60,15 +93,18 @@ namespace Lab6
                 hasPunctuation = true;
                 punctuation = word.Substring(word.Length - 1);
                 word = word.Substring(0, word.Length - 1);
-                word = ExceptionsSpecialCharacters(word, hasPunctuation) + punctuation;
+                //Console.WriteLine("hasPunctuation = " + hasPunctuation);
+                //Console.WriteLine("punctuation = " + punctuation);
+                //word = Exceptions(word);
+                word = ExceptionsSpecialCharacters(word, hasPunctuation, isAllCaps, isTitleCase) + punctuation + "\n";
             }
             else
             {
-                word = ExceptionsSpecialCharacters(word, false);
+                word = ExceptionsSpecialCharacters(word, hasPunctuation, isAllCaps, isTitleCase);
             }
             return word;
         }
-        static string ExceptionsSpecialCharacters(string word, bool hasPunctuation)
+        static string ExceptionsSpecialCharacters(string word, bool hasPunctuation, bool isAllCaps, bool isTitleCase)
         {
             if (Regex.IsMatch(word, "[!@#$%^&*()<>*_+1234567890]+"))
             {
@@ -77,27 +113,45 @@ namespace Lab6
             }
             else
             {
-                word = Regulars(word, hasPunctuation);
+                word = Regulars(word, hasPunctuation, isAllCaps, isTitleCase);
             }
             return word;
         }
 
-        static string Regulars(string word, bool hasPunctuation)
+        static string Regulars(string word, bool hasPunctuation, bool isAllCaps, bool isTitleCase)
         {
             if (StartsAsVowel(word))
             {
                 if (hasPunctuation == true)
                 {
-                    return (word + "way");
+                    if (isAllCaps == true)
+                    {
+                        //Console.WriteLine("input starts with a vowel, ends with punctuation, doesn't have special characters, is all caps, is not titlecase");
+                        return (word + "WAY");
+                    }
+                    else
+                    {
+                        //Console.WriteLine("input starts with a vowel, ends with punctuation, doesn't have special characters, is not all caps, is either titlecase or lowercase");
+                        return (word + "way");
+                    }
                 }
                 else
                 {
-                    return (word + "way ");
+                    if (isAllCaps == true)
+                    {
+                        //Console.WriteLine("input starts with a vowel, doesn't end with punctuation, doesn't have special characters, is all caps, is not titlecase");
+                        return (word + "WAY ");
+                    }
+                    else
+                    {
+                        //Console.WriteLine("input starts with a vowel, doesn't end with punctuation, doesn't have special characters, is not all caps, is either titlecase or lowercase");
+                        return (word + "way ");
+                    }
                 }
             }
             else
             {
-                return TranslateWord(word, hasPunctuation);
+                return TranslateWord(word, hasPunctuation, isAllCaps, isTitleCase);
             }
         }
 
@@ -115,7 +169,7 @@ namespace Lab6
             return false;
         }
 
-        static string TranslateWord(string word, bool hasPunctuation)
+        static string TranslateWord(string word, bool hasPunctuation, bool isAllCaps, bool isTitleCase)
         {
             char[] letters = word.ToCharArray();
             char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
@@ -123,7 +177,7 @@ namespace Lab6
             bool stop = false;
             for (int i = 0; i < word.Length; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 10; j++)
                 {
                     if (letters[i] == vowels[j])
                     {
@@ -139,20 +193,84 @@ namespace Lab6
             }
             if (hasPunctuation == true)
             {
-                string firstHalfOfWord = word.Substring(0, firstVowel);
-                string secondHalfOfWord = word.Substring(firstVowel);
-                word = secondHalfOfWord + firstHalfOfWord + "ay";
-                return word;
+                if (isTitleCase == true)
+                {
+                    string firstHalfOfWord = word.Substring(0, firstVowel);
+                    string secondHalfOfWord = word.Substring(firstVowel);
+                    word = secondHalfOfWord + firstHalfOfWord + "ay";
+                    word = word.ToLower();
+                    word = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word);
+                    //Console.WriteLine("input starts with a consonant, ends with punctuation, doesn't have special characters, is not all caps, is titlecase");
+                    return word;
+
+                }
+                else if (isAllCaps)
+                {
+                    string firstHalfOfWord = word.Substring(0, firstVowel);
+                    string secondHalfOfWord = word.Substring(firstVowel);
+                    word = secondHalfOfWord + firstHalfOfWord + "AY";
+                    //Console.WriteLine("input starts with a consonant, ends with punctuation, doesn't have special characters, is all caps, is not titlecase");
+                    return word;
+                }
+                else
+                {
+                    string firstHalfOfWord = word.Substring(0, firstVowel);
+                    string secondHalfOfWord = word.Substring(firstVowel);
+                    word = secondHalfOfWord + firstHalfOfWord + "ay";
+                    word = word.ToLower();
+                    word = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word);
+                    //Console.WriteLine("input starts with a consonant, ends with punctuation, doesn't have special characters, is not all caps, is not titlecase");
+                    return word;
+                }
             }
             else
             {
-                string firstHalfOfWord = word.Substring(0, firstVowel);
-                string secondHalfOfWord = word.Substring(firstVowel);
-                word = secondHalfOfWord + firstHalfOfWord + "ay ";
-                return word;
+                if (isTitleCase == true)
+                {
+                    string firstHalfOfWord = word.Substring(0, firstVowel);
+                    string secondHalfOfWord = word.Substring(firstVowel);
+                    word = secondHalfOfWord + firstHalfOfWord + "ay ";
+                    word = word.ToLower();
+                    word = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word);
+                    //Console.WriteLine("input starts with a consonant, does not end with punctuation, doesn't have special characters, is not all caps, is titlecase");
+                    return word;
+                }
+                else if (isAllCaps)
+                {
+                    string firstHalfOfWord = word.Substring(0, firstVowel);
+                    string secondHalfOfWord = word.Substring(firstVowel);
+                    word = secondHalfOfWord + firstHalfOfWord + "AY ";
+                    //Console.WriteLine("input starts with a consonant, does not end with punctuation, doesn't have special characters, is all caps, is not titlecase");
+                    return word;
+                }
+                else
+                {
+                    string firstHalfOfWord = word.Substring(0, firstVowel);
+                    string secondHalfOfWord = word.Substring(firstVowel);
+                    word = secondHalfOfWord + firstHalfOfWord + "ay ";
+                    //Console.WriteLine("input starts with a consonant, does not end with punctuation, doesn't have special characters, is not all caps, is not titlecase");
+                    return word;
+                }
             }
 
         }
+
+        static string ActuallyEnteredInput(string input)
+        {
+            if (input.Length > 0)
+            {
+                Console.WriteLine("is translated to...");
+                return input;
+            }
+            else
+            {
+                Console.WriteLine("I didn't detect any input, would you kindly try again");
+                input = Console.ReadLine();
+                input = ActuallyEnteredInput(input);
+            }
+            return input;
+        }
+
         //repeat
         static void Repeat()
         {
@@ -168,21 +286,6 @@ namespace Lab6
                 //Console.ReadLine();
             }
         }
-
-        static string ActuallyEnteredInput(string input)
-        {
-            if (input.Length > 0)
-            {
-                Console.WriteLine("Translated is...");
-                return input;
-            }
-            else
-            {
-                Console.WriteLine("I didn't detect any input, would you kindly try again");
-                input = Console.ReadLine();
-                input = ActuallyEnteredInput(input);
-            }
-            return input;
-        }
     }
 }
+//code by Adam Tasma
